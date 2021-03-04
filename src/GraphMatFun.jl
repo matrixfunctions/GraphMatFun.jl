c146319aec7f0e8d603fafc96296a6e381134916
Elias Jarlebring <jarlebring@gmail.com>
2021-03-04 11:57:03 +0100

    fix for denman beavers

--- src/GraphMatFun.jl
+++ src/GraphMatFun.jl
@@ -4,4 +4,8 @@ include("compgraph_struct.jl")
 include("compgraph_struct_IO.jl")
 include("eval.jl")
 
+
+# Generators
+include("generators/denman_beavers_gen.jl");
+
 end
--- src/generators/denman_beavers_gen.jl
+++ src/generators/denman_beavers_gen.jl
@@ -1,14 +1,17 @@
 """
-        (graph,cref)=gen_denman_beavers(k;T=ComplexF64)
+        (graph,cref)=gen_denman_beavers(k;T=ComplexF64,cref_mode=0)
 
 Creates the graph corresponding to the Denman-Beavers
 iteration for the matrix square root, using k iterations.
+The kwarg `cref_mode` specifies if references
+to all X and Y (`cref_mode=0`) should be stored
+or just Y (`cref_mode=-1`) or just X (`cref_mode=1`).
 
 Reference:
 
   Denman, Eugene D.; Beavers, Alex N. (1976), "The matrix sign function and computations in systems", Applied Mathematics and Computation, 2 (1): 63–94, doi:10.1016/0096-3003(76)90020-5
     """
-function gen_denman_beavers(k;T=ComplexF64)
+function gen_denman_beavers(k;T=ComplexF64,cref_mode)
 
     graph = Compgraph(T);
 
