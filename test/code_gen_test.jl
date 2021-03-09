e3520adc169312c675600279de5b754981ba1695
Elias Jarlebring <jarlebring@gmail.com>
2021-03-09 09:25:35 +0100

    matlab code gen test test

--- test/code_gen_test.jl
+++ test/code_gen_test.jl
@@ -3,10 +3,20 @@ using LinearAlgebra
 
     (graph,crefs)=gen_ps([3 4 0.9 0.11])
     A=[3 4.0; 5.5 0.1];
+
+    # Test julia code generation
     lang=LangJulia(false,false);
     fname=tempname()*".jl";
     gen_code(fname,graph,lang=lang)
+    # and execution
     include(fname);
     @test eval_graph(graph,A)≈dummy(A)
     rm(fname);
+
+    # Test Matlab code generation (not execution)
+    fname=tempname()*".m";
+    gen_code(fname,graph,lang=LangMatlab())
+    rm(fname);
+
+
 end
