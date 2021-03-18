2b1c258f083d545d94d85465db33d6262fedd079
Elias Jarlebring <jarlebring@gmail.com>
2021-03-18 12:36:55 +0100

    dic generator for k=5 #5

--- test/bbc_dic_exp.jl
+++ test/bbc_dic_exp.jl
@@ -40,10 +40,18 @@ using LinearAlgebra
 
 
     x=2.3
-    p5=18 # Order of approx for 5 matrix multiplies
+    p5bbc=18 # Order of approx for 5 matrix multiplies
     (graph,cref)=gen_bbc_basic_exp(5);
     err1=eval_graph(big(graph),big(x))-exp(big(x))
     err2=eval_graph(big(graph),big(x)/α)-exp(big(x)/α)
-    @test -log(abs(err2/err1))/log(α) > p5+1
+    @test -log(abs(err2/err1))/log(α) > p5bbc+1
+
+
+    x=3.2;
+    p5dic=21 # Order of approx for 5 matrix multiplies
+    (graph,cref)=gen_dic_exp(5);
+    err1=eval_graph(big(graph),big(x))-exp(big(x))
+    err2=eval_graph(big(graph),big(x)/α)-exp(big(x)/α)
+    @test -log(abs(err2/err1))/log(α) > p5dic+1
 
 end
