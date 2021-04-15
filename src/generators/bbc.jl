06410d8387fff53c9634420b420dac839519d326
Emil Ringh <eringh@users.noreply.github.com>
2021-04-15 11:38:03 +0200

    extraction of degopt coeffs #14

--- src/generators/bbc.jl
+++ src/generators/bbc.jl
@@ -1,4 +1,4 @@
-export gen_degopt_poly
+export gen_degopt_poly, get_degopt_coeffs
 
 
 """
@@ -114,4 +114,57 @@ function gen_degopt_poly_B(x,T;input=:A)
     end
 
     return (graph,cref);
+end
+
+
+"""
+    (x,z)=get_degopt_coeffs(k;compress_keys=false)
+
+Retruns crefs related to `gen_degopt_poly`. Specifically
+`x` is a `Vector{Tuple{Vector{Tuple{Symbol,Int}},Vector{Tuple{Symbol,Int}}}}`
+such that `x[2][1]` corresponds to the coefficients of the left hand side of the
+multiplication
+
+    B2=(α_2_1 *I + α_2_2 *A)(β_2_1 *I + β_2_2 *A)
+
+i.e., the crefs corresponding to `[α_2_1, α_2_2]`. See `gen_degopt_poly`. Hence,
+`get_coeffs(graph,x[2][1])` returns the corresponding numerical values of the coefficients.
+
+"""
+function get_degopt_coeffs(k;compress_keys=false)
+
+    TT = Tuple{Symbol,Int}
+    x=Vector{Tuple{Vector{TT},Vector{TT}}}(undef,k)
+    for i = 1:k
+        x[i] = ( Vector{TT}(undef,i+1), Vector{TT}(undef,i+1) )
+    end
+
+    for s=2:k+1
+
+        for (b,base) = enumerate(["Ba$s", "Bb$s"])
+            if s == 2
+                x[s-1][b][1] = (Symbol(base), 1)
+            end
+            for i = 2:s-1
+                if i == 2
+                    x[s-1][b][1] = (Symbol("$(base)_2"), 1)
+                end
+                x[s-1][b][i] = (Symbol("$(base)_$(i)"), 2)
+            end
+            x[s-1][b][s] = (Symbol(base), 2)
+        end
+    end
+
+    zlength = compress_keys ? 2 : k+2
+    z = Vector{TT}(undef,zlength)
+    z[1] = (:T2k2, 1)
+    z[2] = (:T2k2, 2)
+    for i = 3:zlength-1
+        z[i] = ( Symbol("T2k$(i)"), 2 )
+    end
+    if !compress_keys
+        z[k+2] = ( Symbol("T2k$(k+3)"), 2 )
+    end
+
+    return (x,z)
 end
--- test/bbc.jl
+++ test/bbc.jl
@@ -49,6 +49,24 @@ using LinearAlgebra
     @test eval_graph(graph,x1)==eval_graph(graph2,x1);
 
 
+    for k = 1:7
+        x=Vector{Tuple{Vector{T},Vector{T}}}(undef,k)
+        for i=1:k
+            x[i]=(collect(1.0:i+1),collect(-1.0:-1:-(i+1)))
+        end
+        z=collect(range(4,5,length=k+2))
+        (graph,cref) = gen_degopt_poly(x,z)
+
+        (xx,zz) = get_degopt_coeffs(k)
+        for i=1:k
+                for N = 1:2
+                    @test get_coeffs(graph,xx[i][N]) == x[i][N]
+                end
+        end
+        for i=1:k+2
+            @test get_coeffs(graph,zz[i])[1] == z[i]
+        end
+    end
 
 
 end
