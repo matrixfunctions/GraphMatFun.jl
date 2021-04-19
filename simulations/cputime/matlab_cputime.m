ee54d40818f3d64f97ccf604ab44d6ac124dcb62
Elias Jarlebring <jarlebring@gmail.com>
2021-04-19 11:54:47 +0200

    update matlab sims

--- simulations/cputime/matlab_cputime.m
+++ simulations/cputime/matlab_cputime.m
@@ -1,6 +1,27 @@
-load("n2000_2_2.mat");
-m=50; tv=zeros(m,1);
+% remember to disable CPU threads boosting:
+% echo "1" > /sys/devices/system/cpu/intel_pstate/no_turbo
+% echo "0" > /sys/devices/system/cpu/intel_pstate/no_turbo
+% from https://askubuntu.com/questions/619875/disabling-intel-turbo-boost-in-ubuntu
+
+%load("n2000_2_2.mat");
+n=2000;
+A0=triu(tril(ones(n,n),3),-3)*1.0 +1.0*eye(n,n);
+%A0=A0-2*spdiags(ones(n,1),-1,n,n);
+
+A0(4,7)=A0(4,7)+eps()*100;
+A0=2.5*A0/norm(A0,1);
+A=A0;
+
+%norm(A)
+%max(abs(eig(A)))
+
+
+m=6;tv=zeros(m,1);
+
+
+"expm native"
 for k=1:m
+    k
     tic;
     expm(A);
     tv(k)=toc;
@@ -11,12 +32,21 @@ mean(tv)
 
 
 
-
 addpath("/tmp/");
-
+"exp_m6_mono_taylor_2_7"
 for k=1:m
     tic;
-    exp_SID_m6(A);
+    exp_m6_mono_taylor_2_7(A);
+    tv(k)=toc;
+end
+
+format long
+mean(tv)
+
+"exp_m6_SID_2_22"
+for k=1:m
+    tic;
+    exp_m6_SID_2_22(A);
     tv(k)=toc;
 end
 
@@ -24,9 +54,21 @@ format long
 mean(tv)
 
 
+"exp_m7_SIDplus_3_59"
 for k=1:m
     tic;
-    exp_SID_m7(A);
+    exp_m7_SIDplus_3_59(A);
+    tv(k)=toc;
+end
+
+format long
+mean(tv)
+
+"expm native"
+for k=1:m
+    k
+    tic;
+    expm(A);
     tv(k)=toc;
 end
 
