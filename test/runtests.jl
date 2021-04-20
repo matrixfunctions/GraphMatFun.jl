c5e0127defe8521459ad3c052b98bcf23c6b92f6
Emil Ringh <eringh@users.noreply.github.com>
2021-04-20 15:08:04 +0200

    added some tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,17 +2,20 @@ using GraphMatFun
 using Test
 
 @testset "GraphMatFun.jl" begin
-    include("bbc.jl");
     include("bbc_sid_exp.jl");
+    include("bbc.jl");
     include("code_gen_test.jl");
     include("compress.jl");
     include("denman_beavers.jl");
+    include("exp.jl");
     include("gauss_newton.jl");
     include("graph_ops.jl");
+    include("import_export.jl");
     include("linear_fit.jl");
     include("merge_graphs.jl");
     include("newton_schulz.jl");
-    include("opt_common");
+    include("opt_common.jl");
     include("polynomials.jl");
     include("polynomials_degopt.jl");
+    include("rational.jl");
 end
