0ee1d51d8fd11c951f1c5c596e313f6aacb39db6
Elias Jarlebring <jarlebring@gmail.com>
2021-04-23 12:47:52 +0200

    cpu-time simulation update

--- simulations/cputime/matlab_cputime.m
+++ simulations/cputime/matlab_cputime.m
@@ -1,20 +1,30 @@
 % remember to disable CPU threads boosting:
-% echo "1" > /sys/devices/system/cpu/intel_pstate/no_turbo
-% echo "0" > /sys/devices/system/cpu/intel_pstate/no_turbo
+% Disable:
+% $ sudo -s
+% # echo "1" > /sys/devices/system/cpu/intel_pstate/no_turbo
+% Enable:
+% $ sudo -s
+% # echo "0" > /sys/devices/system/cpu/intel_pstate/no_turbo
 % from https://askubuntu.com/questions/619875/disabling-intel-turbo-boost-in-ubuntu
 
-display("Note that the turbo should preferrably be disable. No-turbo parameter");
+disp("Note that the turbo should preferrably be disable.");
+disp("  Next line = 1 => Disabled.")
+disp("  Next line =0 => Enabled.");
 type('/sys/devices/system/cpu/intel_pstate/no_turbo')
 
 
-%load("n2000_2_2.mat");
+% Test matrix
 n=2000;
 A0=triu(tril(ones(n,n),3),-3)*1.0 +1.0*eye(n,n);
-%A0=A0-2*spdiags(ones(n,1),-1,n,n);
 
-A0(4,7)=A0(4,7)+eps()*100;
-A0=2.5*A0/norm(A0,1);
-%A0=5.5*A0/norm(A0,1);
+A0(4,7)=A0(4,7)+eps()*100; % Break symmetry to avoid special case code
+col=1;
+if (col==1)
+    A0=2.5*A0/norm(A0,1);
+else
+    A0=5.5*A0/norm(A0,1);
+end
+
 A=A0;
 
 %norm(A)
@@ -36,7 +46,7 @@ format long
 mean(tv)
 pause(2);
 
-
+% Assume generated code is in "/tmp"
 addpath("/tmp/");
 "exp_m6_mono_taylor_2_7"
 for k=1:m
