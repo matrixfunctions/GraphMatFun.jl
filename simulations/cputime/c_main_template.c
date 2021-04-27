175080f76a0d9477bac2185e7d63e7551cbc1c0d
Massimiliano Fasi <massimiliano.fasi@oru.se>
2021-04-27 14:26:11 +0200

    Add C "glue code" to gen_cputime_all_lang.jl (closes #26)

--- simulations/cputime/c_main_template.c
+++ simulations/cputime/c_main_template.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 #include<time.h>
+LOC_INC
+
 #define NOF_SAMPLES 10
 
 int main (int argc, char *argv[]){
@@ -19,61 +21,42 @@ int main (int argc, char *argv[]){
   double sum=0;
   clock_gettime(CLOCK_REALTIME, &begin);
 
+  n = MATSIZE;
+  A = malloc(n * n * sizeof(*A));
+  E = malloc(n * n * sizeof(*A));
 
-  n=MATORDER;
-  // n0=10;
-  A = malloc(n*n*sizeof(*A));
-  E = malloc(n*n*sizeof(*A));
+  // START REPEATED CODE
+  printf("* GRAPH: NAME\n    TIMINGS:  ");
   for(i=0; i<n; i++){
     for(j=0; j<n; j++){
       A[i+n*j]=0.0;
       if (abs(i-j)<4)
-
-	A[i+n*j]=2.5/8.0;
+        A[i+n*j]=2.5/8.0;
       if (i==j)
-	A[i+n*j]=5.0/8.0;
-
+        A[i+n*j]=5.0/8.0;
     }
   }
-//  printf("\n");
-//  for(i=0; i<n0; i++){
-//    for(j=0; j<n0; j++){
-//      printf("%f ",A[i+n*j]);
-//    }
-//    printf("\n");
-//  }
-
-
-  printf("* GRAPH: FUNNAME\n    TIMINGS:  ");
   for (i=0;i<NOF_SAMPLES;i++){
     //tv1=clock();
     clock_gettime(CLOCK_REALTIME, &begin);
-    FUNNAME(A, n, E);
+    dFUNCTION(A, n, E);
     clock_gettime(CLOCK_REALTIME, &end);
 
     //tv2=clock();
     //time = (tv2 - tv1)/(CLOCKS_PER_SEC);
     seconds = end.tv_sec - begin.tv_sec;
     nanoseconds = end.tv_nsec - begin.tv_nsec;
-    elapsed=seconds + nanoseconds*1e-9;
+    elapsed = seconds + nanoseconds*1e-9;
 
     printf("%6.3f ",elapsed);
     //timingv[i] = elapsed;
     sum += elapsed;
-
-
   }
   printf("\n");
   printf("    MEAN: %.3f\n",sum/ NOF_SAMPLES);
-//
-//
-//  printf("\n");
-//  for(i=0; i<n0; i++){
-//    for(j=0; j<n0; j++){
-//      printf("%f ",E[i+n*j]);
-//    }
-//    printf("\n");
-//  }
+
+  // END REPEATED CODE
+
   return 0;
-//
+
 }
--- simulations/cputime/gen_cputime_all_lang.jl
+++ simulations/cputime/gen_cputime_all_lang.jl
@@ -2,21 +2,37 @@ using GraphMatFun;
 include("setup_graphs.jl");
 
 
+# Generate #include preprocessor directives for C code.
+function gen_c_include(names,mkl,skip)
+    blas_string=mkl ? "_MKL" : "_OpenBLAS"
+    include_string=""
+    for name in names
+        if !(name in string.(skip))
+            include_string*="#include\"$(tempdir())/$name$blas_string.c\"\n"
+        end
+    end
+    return include_string
+end
 
-# Process code from template. In common for
-# both matlab and Julia
+# Process code from template.
+# Used for C, julia, and matlab.
 function gen_main(fname,N,col,graphs,names,mkl,commentsign="#",skip=[:expm_matlab,:expmpoly_matlab])
-    #fname="";
+
+    # Common preprocessing:
+    # * include files with generated code (C only);
+    # * replace placeholders that appear only once; and
+    # * find beginning and end of section to be repeated.
     lines=readlines(fname);
     start_repeated_line=0;
     end_repeated_line=0;
     for (i,line)=enumerate(lines)
-        line=replace(line, "COLUMN" => string(col));
+        line=replace(line, "LOC_INC" => gen_c_include(names,mkl,skip)); # C only
+        line=replace(line, "COLUMN" => string(col));       # Julia/Matlab only
         line=replace(line, "MATSIZE" => string(N));
         if (mkl)
-            line=replace(line, "USING" => "using MKL;");
+            line=replace(line, "USING" => "using MKL;");          # Julia only
         else
-            line=replace(line, "USING" => "$commentsign NO MKL");
+            line=replace(line, "USING" => "$commentsign NO MKL"); # Julia only
         end
 
         lines[i]=line
@@ -26,9 +42,7 @@ function gen_main(fname,N,col,graphs,names,mkl,commentsign="#",skip=[:expm_matla
         if contains(line,"END REPEATED CODE")
             end_repeated_line=i;
         end
-
     end
-
 
     repeated_code=lines[start_repeated_line+1:end_repeated_line-1];
 
@@ -43,7 +57,6 @@ function gen_main(fname,N,col,graphs,names,mkl,commentsign="#",skip=[:expm_matla
                 if (g isa Compgraph)
                     line=replace(line,"INCLUDE" =>
                       string("include(joinpath(tempdir(),\"NAME.jl\"));"));
-
                 else
                     line=replace(line,"INCLUDE" => "exp_julia=exp");
                 end
@@ -60,25 +73,20 @@ function gen_main(fname,N,col,graphs,names,mkl,commentsign="#",skip=[:expm_matla
             push!(lines,"$commentsign SKIPPING $g");
             push!(lines,"");
         end
-
-
     end
-
 
     lines=[lines;postprocess]
 
-
     return lines;
-
 end
+
+# Asymptotic number of flops needed to evaluate the a graph.
 function get_cost(graph::Compgraph)
     return count(values(graph.operations) .== :mult)+
            (4/3)*count(values(graph.operations) .== :ldiv)
-
 end
 
-
-
+# Generate functions for evaluating the graphs.
 println("Generating graphs");
 for (i,g)=enumerate(graphs)
     local n;
@@ -95,12 +103,34 @@ for (i,g)=enumerate(graphs)
         # already for C. Since includes etc are different.
         fname_mkl=string(tempdir(),"/$(n)_MKL");
         fname_openblas=string(tempdir(),"/$(n)_OpenBLAS");
-        gen_code("$(fname_mkl).c",g,funname="$(n)_MKL",lang=GraphMatFun.LangC_MKL(),priohelp=priohelp);
-        gen_code("$(fname_openblas).c",g,funname="$(n)_OpenBLAS",lang=GraphMatFun.LangC_OpenBLAS(),priohelp=priohelp);
+        gen_code("$(fname_mkl).c",g,funname="$n",lang=GraphMatFun.LangC_MKL(),priohelp=priohelp);
+        gen_code("$(fname_openblas).c",g,funname="$n",lang=GraphMatFun.LangC_OpenBLAS(),priohelp=priohelp);
 
     end
     println();
 end
+
+# Generate C code.
+println("Generating C/MKL");
+c_template="simulations/cputime/c_main_template.c";
+lines=gen_main(c_template,2000,1,graphs,names,true,"//",
+               [:exp_julia,:expm_matlab,:expmpoly_matlab]);
+open(string(tempdir(),"/run_cputime_c_MKL.c"),"w") do io
+    for line=lines
+        write(io,line,"\n");
+    end
+end
+println("Generating C/OpenBLAS");
+lines=gen_main(c_template,2000,1,graphs,names,false,"//",
+               [:exp_julia,:expm_matlab,:expmpoly_matlab]);
+open(string(tempdir(),"/run_cputime_c_OpenBLAS.c"),"w") do io
+    for line=lines
+        write(io,line,"\n");
+    end
+end
+
+
+# Generate Julia code.
 println("Generating julia/MKL");
 julia_template="simulations/cputime/julia_main_template.jl";
 lines=gen_main(julia_template,2000,1,graphs,names,true);
@@ -117,6 +147,7 @@ open(string(tempdir(),"/run_cputime_julia_OpenBLAS.jl"),"w") do io
     end
 end
 
+# Generate MATLAB code.
 println("Generating matlab/MKL");
 lines=gen_main("simulations/cputime/matlab_main_template.m",2000,1,graphs,names,false,"%",[:exp_julia]);
 open(string(tempdir(),"/run_cputime_matlab_OpenBLAS.m"),"w") do io
