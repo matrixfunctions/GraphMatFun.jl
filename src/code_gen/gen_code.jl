matrixfunctions/GraphMatFun.jl

commit b1b934f535bc39d6a4e2cd4969ec4116bb6cc93a

Fix typo in src/code_gen/gen_code.jl

src/code_gen/gen_code.jl
@@ -88,7 +88,7 @@ function gen_code(fname,graph;
     println(file,to_string(function_init(lang,T,mem,graph)))
 
     println(file,comment(lang,
-                         "Computation order: "*join(string.(order)," ")))
+                         "    Computation order: "*join(string.(order)," ")))
     for (i,node) in enumerate(order)
         (exec_code,result_variable)=execute_operation!(lang,
                                                        T,graph,node,

commit 53215eeaf9527cf3f2a92596f309ff3704a30862

Fix typo introduced in b1b934f

src/code_gen/gen_code.jl
@@ -87,8 +87,8 @@ function gen_code(fname,graph;
     mem=init_mem(lang,nof_slots)
     println(file,to_string(function_init(lang,T,mem,graph)))
 
-    println(file,comment(lang,
-                         "    Computation order: "*join(string.(order)," ")))
+    println(file,"    "*comment(lang,
+                                "Computation order: "*join(string.(order)," ")))
     for (i,node) in enumerate(order)
         (exec_code,result_variable)=execute_operation!(lang,
                                                        T,graph,node,