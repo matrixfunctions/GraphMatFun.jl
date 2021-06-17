d85ef5191a6dd66f572893efe37ca70099fc0bab
Emil Ringh <eringh@users.noreply.github.com>
2021-06-17 11:22:09 +0200

    Another attempt at #47

--- src/code_gen/gen_julia_code.jl
+++ src/code_gen/gen_julia_code.jl
@@ -122,11 +122,15 @@ function function_init(lang::LangJulia,T,mem,graph,precomputed_nodes)
     code=init_code(lang)
     max_nodes=size(mem.slots,1)
 
-    # Allocation
+    # Allocate memory for memory slots.
     push_code!(code,"max_memslots=$max_nodes")
     push_code!(code,"memslots=Vector{Matrix{T}}(undef,max_memslots)")
     push_code!(code,"n=size(A,1)")
-    start_j=1
+    push_code!(code,"for j=1:max_memslots")
+    push_code!(code,"memslots[j]=Matrix{T}(undef,n,n)",ind_lvl=2)
+    push_code!(code,"end")
+
+    # Allocate input.
     push_comment!(code,"The first slots are precomputed nodes $precomputed_nodes")
     for (i,n) in enumerate(precomputed_nodes)
         Ak_slot_name=get_slot_name(mem,n)
@@ -135,31 +139,21 @@ function function_init(lang::LangJulia,T,mem,graph,precomputed_nodes)
             push_code!(code,"$Ak_slot_name=$n "*comment(lang,"overwrite $n"))
         else
             # Otherwise make a copy
-            push_code!(code,"$Ak_slot_name=copy($n)")
+            push_code!(code,"copy!($Ak_slot_name,$n)")
         end
-    end
-
-    if (lang.overwrite_input)
-        start_j += size(precomputed_nodes,1)
     end
 
     # If needed, allocate identity matrix.
     if has_identity_lincomb(graph)
         push_comment!(code,"Graph has linear combination of identities.")
         push_comment!(code,"The matrix I is explicitly allocated.")
-        alloc_slot!(mem,start_j,:I)
+        alloc_slot!(mem,length(precomputed_nodes)+1,:I)
         I_slot_name=get_slot_name(mem,:I)
         push_code!(code,"$I_slot_name=Matrix{Float64}(I,n,n)")
-        start_j+=1
     else
         push_comment!(code,"Uniform scaling is exploited.");
         push_comment!(code,"No matrix I explicitly allocated.")
     end
-
-    # Allocate memory for memory slots.
-    push_code!(code,"for j=$start_j:max_memslots")
-    push_code!(code,"memslots[j]=Matrix{T}(undef,n,n)",ind_lvl=2)
-    push_code!(code,"end")
 
     # If needed, initialize variable of type ValueOne for axpby with a=1 or b=1.
     if (any(map(x->any(x .== 1),values(graph.coeffs))))
