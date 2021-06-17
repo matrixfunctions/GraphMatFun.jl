49fc82da2dec25bf4740021f3ef932bd7435b81d
Emil Ringh <eringh@users.noreply.github.com>
2021-06-17 14:47:09 +0200

    some more on code gen tests

--- test/code_gen_test.jl
+++ test/code_gen_test.jl
@@ -3,18 +3,19 @@ using LinearAlgebra
 
     a = Vector{Number}(undef,4)
     a[1] = 0.11; a[2] = 0.11+0.1im; a[3] = big"0.11"; a[4] = parse(Complex{BigFloat},"0.11+0.1im")
-    A=[3 4.0; 5.5 0.1];
 
     # Test julia code generation
     for i = 1:length(a)
         (graph,crefs)=graph_ps([3 4 2 a[i]])
         for t1 in (true,false)
+            A=[3 4.0; 5.5 0.1];
             lang=LangJulia(t1);
             fname=tempname()*".jl";
-            gen_code(fname,graph,lang=lang)
+            ti = time_ns()
+            gen_code(fname,graph,lang=lang,funname="dummy_$(ti)")
             # and execution
             include(fname);
-            @test eval_graph(graph,A)≈dummy(A)
+            @test eval_graph(graph,A)≈eval(Symbol("dummy_$(ti)"))(A)
             rm(fname);
         end
     end
