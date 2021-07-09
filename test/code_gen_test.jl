9e53ff3b20e4e26deac84f219caa1f4911c79d0a
Emil Ringh <eringh@users.noreply.github.com>
2021-07-09 13:43:51 +0200

    code_gen tests to increase coverage

--- test/code_gen_test.jl
+++ test/code_gen_test.jl
@@ -1,51 +1,57 @@
 using LinearAlgebra
 @testset "code gen" begin
 
-    a = Vector{Number}(undef,4)
-    a[1] = 0.11; a[2] = 0.11+0.1im; a[3] = big"0.11"; a[4] = parse(Complex{BigFloat},"0.11+0.1im")
+    a = Vector{Number}(undef,6)
+    a[1] = 0.11; a[2] = 0.11+0.1im
+    a[3] = Float32(0.11); a[4] = ComplexF32(0.11+0.1im)
+    a[5] = big"0.11"; a[6] = parse(Complex{BigFloat},"0.11+0.1im")
 
     # Test julia code generation
     for i = 1:length(a)
         (graph,crefs)=graph_ps([3 4 2 a[i]])
+        add_ldiv!(graph,:R0,:A2,:P0)
+        clear_outputs!(graph)
+        add_output!(graph,:R0)
         for t1 in (true,false)
-            A=[3 4.0; 5.5 0.1];
-            lang=LangJulia(t1);
-            fname=tempname()*".jl";
+            A=[3 4.0; 5.5 0.1]
+            lang=LangJulia(t1)
+            fname=tempname()*".jl"
             ti = time_ns()
             gen_code(fname,graph,lang=lang,funname="dummy_$(ti)")
             # and execution
-            include(fname);
+            include(fname)
             @test eval_graph(graph,A)≈eval(Symbol("dummy_$(ti)"))(A)
-            rm(fname);
+            rm(fname)
         end
     end
 
-    (graph,crefs)=graph_ps([3 4 2 a[1]])
-    # Test Matlab code generation (not execution)
-    fname=tempname()*".m";
-    gen_code(fname,graph,lang=LangMatlab())
-    rm(fname);
+    for i = 1:4 #Not high-precision test for Matlab and C
+        (graph,crefs)=graph_ps([3 4 2 a[i]])
+        add_ldiv!(graph,:R0,:A2,:P0)
+        clear_outputs!(graph)
+        add_output!(graph,:R0)
 
-    # Test C code generation
+        # Test Matlab code generation (not execution)
+        fname=tempname()*".m"
+        gen_code(fname,graph,lang=LangMatlab())
+        rm(fname)
 
-    fname=tempname()*".c";
-    gen_code(fname,graph,lang=LangC_MKL())
-    rm(fname);
+        # Test C code generation
+        fname=tempname()*".c"
+        gen_code(fname,graph,lang=LangC_MKL())
+        rm(fname)
 
-    fname=tempname()*".c";
-    gen_code(fname,graph,lang=LangC_MKL(true))
-    rm(fname);
+        fname=tempname()*".c"
+        gen_code(fname,graph,lang=LangC_MKL(true))
+        rm(fname)
 
-    fname=tempname()*".c";
-    gen_code(fname,graph,lang=LangC_OpenBLAS())
-    rm(fname);
+        fname=tempname()*".c"
+        gen_code(fname,graph,lang=LangC_OpenBLAS())
+        rm(fname)
 
-    fname=tempname()*".c";
-    gen_code(fname,graph,lang=LangC_OpenBLAS(true))
-    rm(fname);
-
-
-
-
+        fname=tempname()*".c"
+        gen_code(fname,graph,lang=LangC_OpenBLAS(true))
+        rm(fname)
+    end
 
 end
