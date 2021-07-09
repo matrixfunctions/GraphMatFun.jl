e3d76020fd6f1dd100e411bcbbb190beb93529b5
Emil Ringh <eringh@users.noreply.github.com>
2021-07-09 09:43:03 +0200

    code style

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,24 +2,24 @@ using GraphMatFun
 using Test
 
 @testset "GraphMatFun.jl" begin
-    include("bbc_sid_exp.jl");
-    include("bbcs_cheb_exp.jl");
-    include("code_gen_test.jl");
-    include("compress.jl");
-    include("degopt_poly.jl");
-    include("degopt.jl");
-    include("denman_beavers.jl");
-    include("exp.jl");
-    include("gauss_newton.jl");
-    include("graph_ops.jl");
-    include("import_export.jl");
-    include("linear_fit.jl");
-    include("merge_graphs.jl");
-    include("newton_schulz.jl");
-    include("opt_common.jl");
-    include("polynomials_degopt.jl");
-    include("polynomials.jl");
-    include("rational.jl");
-    include("sastre.jl");
-    include("topo_order.jl");
+    include("bbc_sid_exp.jl")
+    include("bbcs_cheb_exp.jl")
+    include("code_gen_test.jl")
+    include("compress.jl")
+    include("degopt_poly.jl")
+    include("degopt.jl")
+    include("denman_beavers.jl")
+    include("exp.jl")
+    include("gauss_newton.jl")
+    include("graph_ops.jl")
+    include("import_export.jl")
+    include("linear_fit.jl")
+    include("merge_graphs.jl")
+    include("newton_schulz.jl")
+    include("opt_common.jl")
+    include("polynomials_degopt.jl")
+    include("polynomials.jl")
+    include("rational.jl")
+    include("sastre.jl")
+    include("topo_order.jl")
 end
