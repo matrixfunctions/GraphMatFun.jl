110487ccdd6b079d0f469914c2a9d1c539e424ae
Massimiliano Fasi <massimiliano.fasi@oru.se>
2021-07-17 18:55:52 +0200

    Fix spacing in generated MATLAB code

--- src/code_gen/gen_matlab_code.jl
+++ src/code_gen/gen_matlab_code.jl
@@ -16,9 +16,9 @@ slotname(::LangMatlab,i)="memslots{$i}"
 
 function assign_coeff(::LangMatlab,v,i)
     if imag(v) == 0
-        ("coeff$i","coeff$i=$(v)")
+        ("coeff$i","coeff$i = $(v)")
     else
-        ("coeff$i","coeff$i=$(real(v)) + 1i*$(imag(v))")
+        ("coeff$i","coeff$i = $(real(v)) + 1i*$(imag(v))")
     end
 end
 
@@ -28,14 +28,14 @@ end
 function function_definition(lang::LangMatlab,graph,T,funname,precomputed_nodes)
     code=init_code(lang)
     input_variables=join(precomputed_nodes, ", ");
-    push_code!(code,"function output=$funname($input_variables)",ind_lvl=0)
+    push_code!(code,"function output = $funname($input_variables)",ind_lvl=0)
     return code
 end
 
 function function_init(lang::LangMatlab,T,mem,graph,precomputed_nodes)
     code=init_code(lang)
-    push_code!(code,"n=size(A,1);")
-    push_code!(code,"I=eye(n,n);")
+    push_code!(code,"n = size(A,1);")
+    push_code!(code,"I = eye(n,n);")
     return code
 end
 
@@ -46,7 +46,7 @@ end
 
 function function_end(lang::LangMatlab,graph,mem)
     code=init_code(lang)
-    push_code!(code,"output=$(graph.outputs[end]);")
+    push_code!(code,"output = $(graph.outputs[end]);")
     push_code!(code,"end",ind_lvl=0)
     return code
 end
@@ -63,15 +63,15 @@ function execute_operation!(lang::LangMatlab,
     push_comment!(code,"Computing $node with operation: $op")
     # Needs to be updated
     if op == :mult
-        push_code!(code,"$node=$parent1 * $parent2;")
+        push_code!(code,"$node = $parent1 * $parent2;")
     elseif op == :ldiv
-        push_code!(code,"$node=$parent1 \\ $parent2;")
+        push_code!(code,"$node = $parent1 \\ $parent2;")
     elseif op == :lincomb
         (coeff1,coeff1_code)=assign_coeff(lang,graph.coeffs[node][1],1)
         push_code!(code,"$coeff1_code;")
         (coeff2,coeff2_code)=assign_coeff(lang,graph.coeffs[node][2],2)
         push_code!(code,"$coeff2_code;")
-        push_code!(code,"$node= $coeff1*$parent1+$coeff2*$parent2;")
+        push_code!(code,"$node = $coeff1*$parent1 + $coeff2*$parent2;")
     end
     return (code,"$node")
 end
