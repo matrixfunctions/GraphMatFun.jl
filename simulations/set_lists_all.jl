5c39ecf349fc315b70d4dd3395a8ef96fa635b1d
Elias Jarlebring <jarlebring@gmail.com>
2021-07-19 08:29:22 +0200

    moved simulations. Closes #48

--- simulations/set_lists_all.jl
+++
@@ -1,3 +0,0 @@
-# All the simulations
-graph_list=[graph_mono1,graph_mono2,graph_ps1,graph_ps2,graph_sastre,graph_bbc,graph_sid,sastre_org,bbc_org,sid_org];
-name_list=["mono_taylor","mono_lsqr","PS_taylor","PS_lsqr","Sastre+","BBC+","SID+","Sastre","BBC","SID"];
