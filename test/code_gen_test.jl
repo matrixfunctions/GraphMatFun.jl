bdb71cd53c64229559f49c095b1308c519855e05
Elias Jarlebring <jarlebring@gmail.com>
2024-01-04 13:52:26 +0100

    get rid of warnings #74

--- test/code_gen_test.jl
+++ test/code_gen_test.jl
@@ -39,23 +39,28 @@ using LinearAlgebra, StaticArrays
     # Test Statically sized matrix
     (graph, crefs) = graph_ps([3 4 2 10.0])
     fname = tempname() * ".jl"
-    gen_code(fname, graph, funname = "thisfunction")
-    # and execution
-    include(fname)
-    rm(fname)
+    begin
+        gen_code(fname, graph, funname = "thisfunction")
+        # and execution
+        include(fname)
+        rm(fname)
 
-    A=[  0.704017  -0.358008   0.0649856;  -1.50413    0.0251634  0.0170085;  0.348764   0.334011   0.0915101]
-    Am=MMatrix{3,3}(A);
-    @test thisfunction(A)≈thisfunction(Am)
+        A=[  0.704017  -0.358008   0.0649856;  -1.50413    0.0251634  0.0170085;  0.348764   0.334011   0.0915101]
+        Am=MMatrix{3,3}(A);
+        @test thisfunction(A)≈thisfunction(Am)
+    end
 
     # Test precomputed nodes
     fname = tempname() * ".jl"
-    gen_code(fname, graph, funname = "thisfunction2",precomputed_nodes=[:A,:A2])
-    # and execution
-    include(fname)
-    rm(fname)
-    @test thisfunction2(Am,Am*Am)≈thisfunction(Am)
+    begin
+        gen_code(fname, graph, funname = "thisfunction2",precomputed_nodes=[:A,:A2])
+        # and execution
+        include(fname)
+        rm(fname)
+        @test thisfunction2(Am,Am*Am)≈thisfunction(Am)
+    end
 
+    @show "testing other langs"
     for i = 1:4 #Not high-precision test for Matlab and C
         (graph, crefs) = graph_ps([3 4 2 a[i]])
         add_ldiv!(graph, :R0, :A2, :P0)
