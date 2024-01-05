02f00a1445708b70017582e8a16dea0b4389b07c
Elias Jarlebring <jarlebring@gmail.com>
2024-01-05 11:12:51 +0100

    clean up zero coeff compress for mult lincomb

--- src/compress_graph.jl
+++ src/compress_graph.jl
@@ -61,7 +61,7 @@ end
 Searches for linear combinations with zero coeff and removes those. The cref
 list deletes references to zero coeffs and updates all other crefs.
 """
-function compress_graph_zero_coeff_new!(
+function compress_graph_zero_coeff!(
     graph,
     cref = [];
     droptol = 0,
